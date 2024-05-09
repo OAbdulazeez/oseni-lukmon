f610316eb82c5633cdf038043740e12d323effd9
Olagunju Abdulazeez Olayinka <olagunjuabdulazeez28@gmail.com>
2024-05-09 15:47:52 +0100

    footer added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     extend: {
       backgroundImage: {
         headerbg: "url('/src/images/oseni-header-image.jpg')",
+        ourprojectbg: "url('/src/images/lagos state revenue board.jpeg')",
       },
       fontFamily: {
         custom: ["Bodoni Moda", "sans"], // Add your font family name here
